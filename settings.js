8983bf845bc69135675eb2567784817d497134f8
stone <719045032@qq.com>
2015-10-25 19:55:02 +0800

    mongodb-address

--- settings.js
+++ settings.js
@@ -1,15 +1,16 @@
 /**
  * Created by shijin on 2015/9/24.
  */
-/*module.exports = {
+module.exports = {
     cookieSecret: 'microblogbyvoid',
     db: 'stone',
     host: 'ds051953.mongolab.com',
     port: "51953"
-};*/
+};
+/*
 module.exports = {
     cookieSecret: 'microblogbyvoid',
     db: 'stone',
     host: 'localhost',
     port: '27017'
-};
+};*/
